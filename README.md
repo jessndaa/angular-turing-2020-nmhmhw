9dd19b80fd9ed7385f3d14f223b8685fb5219014
jessndaa <jessyndaya15@gmail.com>
2021-07-31 16:07:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-turing-2020-nmhmhw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-turing-2020-nmhmhw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /**
+
+   * The name of the application
+   */
+  name: string = 'Welcome to Turing Angular Interview';
+  arr = [0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1];
+
+  getConcecutiveOne(){
+    const allCons: Array<number> = [];
+    var cons = 0;
+    for(let i = 0; i< this.arr.length; i++ ){
+      if(this.arr[i] === 0){
+        cons = 0
+      } else {
+        console.log(cons)
+        allCons.push(++cons);
+      }
+    }
+    var c = 0
+    for(let y of allCons){
+      if(y>c){
+        c = y
+      }
+    }
+    
+    return c;
+  }
+}
